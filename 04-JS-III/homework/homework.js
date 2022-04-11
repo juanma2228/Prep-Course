88210a9edcd98a8532aab758d945c2c3ec1dc3de
juanma2228 <juanmacolaneri@outlook.com>
2022-04-11 13:22:23 -0300

    Completo

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -206,6 +206,19 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var iterarMasDos = [];
+  var suma = numero;
+  for (var i = 0; i < 10; i++) {
+    suma = suma + 2;
+    if (suma === i) break;
+    else {iterarMasDos.push(suma)
+    };
+  }
+  if (i < 10) {
+    return "Se interrumpió la ejecución"
+  }
+  else {return iterarMasDos;
+  }
 }
 
 
@@ -216,6 +229,16 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var array = [];
+  var suma = numero;
+  for (let i = 0; i < 10; i++) {
+    if (i === 5) continue;
+    else {
+      suma = suma + 2;
+      array.push(suma)
+    }
+  }
+  return array
 }
 
 
